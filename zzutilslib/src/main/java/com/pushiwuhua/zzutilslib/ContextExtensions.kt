9c3db29229910f20a6a9989fe7d183243cc88903
wangzhenzhi <wangzhenzhi@orieange.com>
2019-12-31 14:55:25 +0800

    完善Context的扩展方法

--- zzutilslib/src/main/java/com/pushiwuhua/zzutilslib/ContextExtensions.kt
+++ zzutilslib/src/main/java/com/pushiwuhua/zzutilslib/ContextExtensions.kt
@@ -18,6 +18,8 @@ import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
 import android.provider.Settings
+import android.telephony.TelephonyManager
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -38,6 +40,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeoutOrNull
 import java.net.URL
+import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -375,12 +378,74 @@ fun Context.getVerName(): String? {
 /**
  * 获取App名称
  */
-fun getAppName(context: Context): String? {
-    return context.resources.getText(
-        context.resources.getIdentifier(
+fun Context.getAppName(): String? {
+    return resources.getText(
+        resources.getIdentifier(
             "app_name",
             "string",
-            context.packageName
+            packageName
         )
     ).toString()
+}
+
+/**
+ * deviceID的组成为：渠道标志+识别符来源标志+hash后的终端识别符
+ *
+ *
+ * 渠道标志为：
+ * 1，andriod（a）
+ *
+ *
+ * 识别符来源标志：
+ * 1， wifi mac地址（wifi）；
+ * 2， IMEI（imei）；
+ * 3， 序列号（sn）；
+ * 4， id：随机码。若前面的都取不到时，则随机生成一个随机码，需要缓存。
+ *
+ * @param context
+ * @return
+ */
+fun Context.getDeviceId(): String? {
+    val deviceId = StringBuilder()
+    // 渠道标志
+    deviceId.append("ut")
+    try {
+        val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val imei = tm.deviceId
+        if (!TextUtils.isEmpty(imei)) {
+            deviceId.append("imei")
+            deviceId.append(imei)
+            return deviceId.toString()
+        }
+        //序列号（sn）
+        val sn = tm.simSerialNumber
+        if (!TextUtils.isEmpty(sn)) {
+            deviceId.append("sn")
+            deviceId.append(sn)
+            return deviceId.toString()
+        }
+        //如果上面都没有， 则生成一个id：随机码
+        val uuid = getUUID()
+        if (!TextUtils.isEmpty(uuid)) {
+            deviceId.append("id")
+            deviceId.append(uuid)
+            return deviceId.toString()
+        }
+    } catch (e: java.lang.Exception) {
+        e.printStackTrace()
+        deviceId.append("id").append(getUUID())
+    }
+    return deviceId.toString()
+}
+
+/**
+ * 得到全局唯一UUID
+ */
+fun Context.getUUID(): String? {
+    var uuid = Utils.getPreferenceString(this, "uuid")
+    if (TextUtils.isEmpty(uuid)) {
+        uuid = UUID.randomUUID().toString()
+        Utils.savePreferenceString(this, "uuid", uuid)
+    }
+    return uuid
 }
