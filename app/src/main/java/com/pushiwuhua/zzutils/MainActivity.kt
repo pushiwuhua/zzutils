8031be1826c31af3263a77daa97685e4d79666e9
wangzhenzhi <wangzhenzhi@orieange.com>
2020-01-03 18:02:46 +0800

    增加下载文件的工具类 KDownQk

--- app/src/main/java/com/pushiwuhua/zzutils/MainActivity.kt
+++ app/src/main/java/com/pushiwuhua/zzutils/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.pushiwuhua.zzutilslib.KDownQk
 import com.pushiwuhua.zzutilslib.KOneFilter
 import com.pushiwuhua.zzutilslib.KWaitingDialog
 import com.pushiwuhua.zzutilslib.singleClick
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
+import okhttp3.Call
+import java.io.File
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +23,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        var call: Call? = null
+        btnDown.singleClick {
+            Log.i("wzz", "MainActivity onCreate 线程${Thread.currentThread()}")
+            call = KDownQk.downFile(
+                URL("https://ossn.cmfun.cn/group1/M00/00/01/CgsANl4O9n-ANiTdAnTs1bcvWIY653.apk"),
+                File(getExternalFilesDir("").absolutePath + "/ttt${System.currentTimeMillis()}.apk"),
+                object : KDownQk.Status {
+                    override fun err(err: Throwable) {
+                        Log.i("wzz", "MainActivity err $err")
+                    }
+
+                    override fun finish() {
+                        Log.i("wzz", "MainActivity finish ")
+                        btnDown.text = "下载"
+                    }
+
+                    override fun start() {
+                        Log.i("wzz", "MainActivity start ${Thread.currentThread()}")
+                    }
+
+                    override fun downing(percents: Float) {
+                        Log.i("wzz", "MainActivity downing $percents ${Thread.currentThread()}")
+                        lifecycleScope.launch {
+                            Log.i("wzz", "MainActivity downing 内部 $percents ${Thread.currentThread()}")
+                            btnDown.text = "下载$percents"
+                        }
+                    }
+                }
+            )
+        }
+
+        btnCancel.singleClick {
+            Log.i("wzz", "MainActivity onCreate 取消下载")
+            call?.cancel()
+        }
+
         fab.singleClick {
             Log.i("wzz", "MainActivity onCreate2 ")
-            KWaitingDialog.open(lifecycleScope,supportFragmentManager,"haha")
+            KWaitingDialog.open(lifecycleScope, supportFragmentManager, "haha")
         }
         val filter: KOneFilter<Boolean> = KOneFilter()
         Log.i("wzz", "MainActivity onCreate ${filter.out(true)}")
---
+++ zzutilslib/src/main/java/com/pushiwuhua/zzutilslib/KDownQk.kt
@@ -0,0 +1,94 @@
+@file:Suppress("unused")
+
+package com.pushiwuhua.zzutilslib
+
+import android.util.Log
+import androidx.annotation.UiThread
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.net.URL
+
+
+/**
+ * 封装OkHttp的下载文件类
+ * wzz created at 2020/1/3 14:16
+ */
+object KDownQk {
+    private val okClient: OkHttpClient by lazy {
+        OkHttpClient()
+    }
+
+    /**
+     * 下载状态
+     */
+    interface Status {
+        fun err(err: Throwable)
+        fun finish()
+        fun start()
+        fun downing(percents: Float)
+    }
+
+    /**
+     * 下载文件
+     */
+    fun downFile(url: URL, file: File, downStatus: Status): Call {
+        val request: Request = Request.Builder().url(url).build()
+        return okClient.newCall(request).also {
+            it.enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    downStatus.err(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.i("wzz", "KDownQk onResponse 线程 ${Thread.currentThread()}")
+                    writeFile(response, file, downStatus)
+                }
+            })
+        }
+    }
+
+    /**
+     * 写入文件
+     */
+    private fun writeFile(response: Response, outFile: File, downStatus: Status) {
+        Log.i("wzz", "KDownQk writeFile $outFile")
+        val ins = response.body?.byteStream()
+        val fos = FileOutputStream(outFile)
+        kotlin.runCatching {
+            val bytes = ByteArray(1024)
+            var len = 0
+            //获取下载的文件的大小
+            val fileSize = response.body!!.contentLength()
+            downStatus.start()
+            Log.i("wzz", "KDownQk writeFile 文件总大小 $fileSize")
+            var sum: Long = 0
+            var porSize: Int
+            while (ins?.read(bytes).also { it?.let { len = it } } != -1) {
+                Log.i("wzz", "KDownQk writeFile 每一次读写 $len")
+                fos.write(bytes, 0, len)
+                sum += len.toLong()
+                porSize = (sum * 1.0f / fileSize * 100).toInt()
+                Log.i("wzz", "KDownQk writeFile 百分比 $fileSize $sum $porSize")
+                downStatus.downing(porSize.toFloat())
+            }
+        }.onFailure {
+            Log.i("wzz", "KDownQk writeFile 失败 $it")
+            ins?.close()
+            fos.close()
+            downStatus.err(it)
+        }
+            .onSuccess {
+                Log.i("wzz", "KDownQk writeFile 成功")
+                Log.i("wzz", "KDownQk writeFile 线程 ${Thread.currentThread()}")
+                ins?.close()
+                fos.flush()
+                fos.close()
+                downStatus.finish()
+            }
+    }
+
+}
